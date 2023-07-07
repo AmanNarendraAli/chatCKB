5f5ac1f247e2b9049d32a6291df731355cc8bf80
AmanNarendraAli <amannarendraali@gmail.com>
2023-07-07 11:16:28 +0530

    added splitter to txtread.py

--- txtread.py
+++ txtread.py
@@ -1,4 +1,8 @@
 import os
+from PyPDF2 import PdfReader 
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import ElasticVectorSearch,Pinecone,Weaviate,FAISS
 
 os.environ["OPENAI_API_KEY"] = "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"
 
@@ -9,7 +13,17 @@ def get_text_file_content(txt_files):
             text += file.read()
     return text
 
-txt_files = ["/Users/vir/Documents/GitHub/chatCKB/premchand.txt"]
+def get_text_chunks(raw_text):
+    splitter = CharacterTextSplitter(
+        separator = "\n",
+        chunk_size = 1000,
+        chunk_overlap = 100,
+        length_function = len
+    )
+    chunks = splitter.split(raw_text)
+    return chunks
+
+txt_files = ["testdoc.txt"]
 raw_text = get_text_file_content(txt_files)
 
 print(raw_text)

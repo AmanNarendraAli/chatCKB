1774f9cd31c57d5e571542ca727b61375c759278
AmanNarendraAli <amannarendraali@gmail.com>
2023-07-07 12:09:03 +0530

    forgot to add one line

--- txtread.py
+++ txtread.py
@@ -31,6 +31,7 @@ def get_vectorstore(chunks):
     return vectorstore
 
 def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
     memory = ConversationBufferMemory(memory_key="ChatHistory",return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm,retriever=vectorstore.as_retriever(),memory=memory)
     return conversation_chain

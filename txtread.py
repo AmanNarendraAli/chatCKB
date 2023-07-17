39d65d5670aec1035462642824436e17aba1c07e
v-khanna <102773390+v-khanna@users.noreply.github.com>
2023-07-17 15:20:53 +0530

    unscrew the url screw you

--- txtread.py
+++ txtread.py
@@ -12,6 +12,7 @@ import openai
 import requests
 from bs4 import BeautifulSoup
 import validators
+
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 def read_pdf(pdf_name):
@@ -104,11 +105,20 @@ def get_text_from_url(url):
 def main():
     pdf_docs = get_pdfs()
     raw_text = read_pdf("allminutes.pdf")
-    query = input("Enter your query: ")
+    input_string = input('Enter your question: ')
+
+    # Check if input string is a URL
+    if validators.url(input_string):
+        # If it's a URL, extract the text from the webpage
+        raw_text += get_text_from_url(input_string)
+        query = input("Actually enter your question: ")
+    else:
+        # If it's not a URL, consider it as the query
+        query = input_string
+
     chunks = get_text_chunks(raw_text)
     vectorstore = get_vectorstore(chunks)
     conversation_chain = get_conversation_chain(vectorstore)
-
     chat_history = []  # Add this line if there's no chat history yet.
     response = conversation_chain.run({"question":query,"chat_history":chat_history})
     print(response)

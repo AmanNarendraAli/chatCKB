120162ac94bff2e0d8d0d20131b29efe46481a12
v-khanna <vkhanna@udavis.edu>
2023-07-31 11:17:39 +0530

    i think it should be working - separated main.py and slackbot.py, initialised flask and some other stuff

--- slack_bot.py
+++ slack_bot.py
@@ -7,7 +7,17 @@ import json
 import validators
 import requests
 from bs4 import BeautifulSoup
-from main import handle_single_message, get_conversation_chain, get_vectorstore, get_text_chunks, read_pdf, get_pdfs, get_text_from_url
+from main import (
+    handle_single_message,
+    get_conversation_chain,
+    get_vectorstore,
+    get_text_chunks,
+    read_pdf,
+    get_pdfs,
+    get_text_from_url,
+    load_chat_history,
+    save_chat_history,
+)
 
 # Get the slack token and signing secret from the environment
 slack_token = os.getenv("SLACK_BOT_TOKEN")
@@ -24,16 +34,22 @@ app = Flask(__name__)
 
 # Global variable to store the conversation chain
 conversation_chain = None
+chat_history_filename = "chat_history.json"  # The filename for storing chat history
 
-@app.route('/slack/message_actions', methods=['POST'])
+
+@app.route("/slack/message_actions", methods=["POST"])
 def message_actions():
     # Verify the request
-    if not signature_verifier.is_valid_request(request.get_data().decode('utf-8'), request.headers):
+    if not signature_verifier.is_valid_request(
+        request.get_data().decode("utf-8"), request.headers
+    ):
         return make_response("invalid request", 403)
 
     # Extract the user's message
     form_json = json.loads(request.form["payload"])
-    user_message = form_json['message']['blocks'][0]['elements'][0]['elements'][0]['text']
+    user_message = form_json["message"]["blocks"][0]["elements"][0]["elements"][0][
+        "text"
+    ]
 
     global conversation_chain
     if conversation_chain is None:
@@ -42,7 +58,9 @@ def message_actions():
 
         # Check if there are any PDFs in the ckbDocs folder
         if len(pdf_docs) == 0:
-            print("No PDF files found in ckbDocs folder. Please enter a URL to proceed.")
+            print(
+                "No PDF files found in ckbDocs folder. Please enter a URL to proceed."
+            )
             url = input("Enter the URL: ")
 
             # Check if input string is a valid URL
@@ -62,14 +80,20 @@ def message_actions():
         vectorstore = get_vectorstore(chunks)
         # Generate a conversation chain from the vector store
         conversation_chain = get_conversation_chain(vectorstore)
+        print("Initialized conversation chain")  # Debug print statement
 
     # Get the response from our bot
-    bot_response = handle_single_message(conversation_chain, user_message)
+    bot_response = handle_single_message(
+        conversation_chain, user_message, chat_history_filename
+    )
 
     # Send the response back to Slack
-    slack_web_client.chat_postMessage(channel=form_json['channel']['id'], text=bot_response)
+    slack_web_client.chat_postMessage(
+        channel=form_json["channel"]["id"], text=bot_response
+    )
 
     return make_response("", 200)
+
 
 if __name__ == "__main__":
     app.run(port=3000)
